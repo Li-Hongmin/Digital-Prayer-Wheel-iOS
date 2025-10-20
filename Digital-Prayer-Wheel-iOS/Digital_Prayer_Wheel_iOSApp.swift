75e4bdddf4b2c1cfbac804e1432cffc325812c48
YunQiAI <excerpt.10.able@icloud.com>
2025-10-20 11:55:57 +0900

    Initial Commit

---
+++ Digital-Prayer-Wheel-iOS/Digital_Prayer_Wheel_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Prayer_Wheel_iOSApp.swift
+//  Digital-Prayer-Wheel-iOS
+//
+//  Created by Li Hongmin on 2025/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Prayer_Wheel_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
